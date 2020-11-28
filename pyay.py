bd1bb706caf503d8b4d56d5b74125d46cea9b2bc
ArticOceanWater <ArticOceanWater@protonmail.ch>
2020-11-28 23:40:08 +0530

    Properly implemented install and search featues in yay-wrapper

--- pyay.py
+++ pyay.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import subprocess
 from shlex import quote
+
+def install(packages):
+    pacman("-S",packages)
+
+def search(packages):
+    pacman("-Ss",packages)
+
+def pacman(flags,pkgs):
+    cmd=["yay","--sudoloop",flags]
+    for s in pkgs:
+        cmd+=[s]
+    subprocess.run(cmd)
+
